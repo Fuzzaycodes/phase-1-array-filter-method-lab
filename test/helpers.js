ca21505d6b3249251663d3c7d781a05386c0984b
Fuzzay <samomwerif2@gmail.com>
2024-07-22 14:59:53 +0300

    filter

--- test/helpers.js
+++ test/helpers.js
@@ -18,3 +18,22 @@ const src = babelResult.code
 jsdom({
   html, src
 });
+
+
+function findMatching(drivers, name) {
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name) {
+  return drivers.filter(driver => driver.name === name);
+}
+
+module.exports = {
+  findMatching,
+  fuzzyMatch,
+  matchName
+};
